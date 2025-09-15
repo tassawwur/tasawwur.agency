7b78211a01e1a2a80fa513f9de51539775d3be06
Tassawwur Hussain Joiya <joiyaseditz@gmail.com>
2025-09-15 17:58:25 +0500

    lenis property fix

--- src/lib/smooth-scroll.ts
+++ src/lib/smooth-scroll.ts
@@ -17,11 +17,6 @@ export function initSmoothScroll() {
   lenis = new Lenis({
     duration: 1.2,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-    smooth: true,
-    mouseMultiplier: 1,
-    smoothTouch: false,
-    touchMultiplier: 2,
-    infinite: false,
   })
 
   // RAF loop
